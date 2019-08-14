330d895567ea635035a65a6ec05baf0b35fc3910
hcgaron <hcgaron@gmail.com>
2019-08-13 23:22:59 -0400

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for EXPRESS config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -80,45 +81,6 @@ app.get('/weather', (req, res) => {
   })
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-app.get('/products', (req, res) => {
-  if (!req.query.search) {
-    return res.send({
-      error: 'You must provide a serach term'
-    });
-  }
-  console.log(req.query.search);
-  res.send({
-    products: []
-  });
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // help subdir 404
 app.get('/help/*', (req, res) => {
   res.render('404', {
@@ -139,6 +101,6 @@ app.get('*', (req, res) => {
 
 
 // set server listening
-app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
 })
